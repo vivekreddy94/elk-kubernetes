31fd99d89f2d38ea47e0f870b725e4df111b79af
Ubuntu <vivek@k8snode.41mazynvbylerpmoxzhtd12vab.bx.internal.cloudapp.net>
2020-10-04 17:15:20 +0000

    Source code for elk deployment on kubernetes

---
+++ README.md
@@ -0,0 +1,157 @@
+# ELK stack on kubernetes cluster
+Elasticsearch, Logstash, Kibana and filebeat installation on kubernetes cluster using Jenkins CICD pipeline.
+
+## Prerequisites
+* kubernetes
+* Ansible >= 2.9 version
+* Docker
+
+## Jenkins Installation
+Jenkins installation is done via ansible-playbook with a custom docker image and making use of jenkins configuration as code plugin.
+### Jenkins image
+Dockerfile includes installation of
+* Jenkins basic suggested plugins along with plugins like git, kubernetes, job-dsl etc
+* Docker installation to enable docker commands inside Jenkins
+* Ansible package
+* kubectl package
+### Jenkins slave image
+A custom slave image is built from official jnlp image. This is important as we run all our ELK stack playbooks here. Similar to Jenkins image, Dockerfile consists of below 
+* Docker
+* Ansible
+* kubectl
+* kubeval
+* polaris
+### Jenkins configuration
+Configuration is completely managed through Jenkins configuration as code plugin. It includes
+* Setting up user access.
+* Creates kubernetes cloud and adding pod template with our custom jenkins slave image in it
+* Create our first job for ELK CICD pipeline with github hook enabled.
+### Before running jenkins playbook
+- Copy kubeconfig file to user home directory path("~/.kube/config") if not already present.
+- Update below variables in jenkins inventory file
+  * kubernetes api server address
+  * **kubeconfig_file** - Convert file into base64 format before adding to inventory. 
+   ```
+   cat ~/.kube/config | base64 -w 0
+   ```
+  * **docker_login_username** - Docker hub login username
+  * **docker_login_password**  - Docker hub login password
+  * **github_repo_token** - Token to github repository
+  * **adminuser_password** - Password for logging to Jenkins instance as "admin" user.
+
+If needed other variable values can be changed based on requirement.
+
+Note: For encrypting passwords and kubeconfig content before adding to inventory file, follow https://docs.ansible.com/ansible/latest/user_guide/vault.html#creating-encrypted-variables
+
+### Running Jenkins playbook
+Execute below ansible command and it should prompt for the vault password which is used previously to encrypt strings.
+```
+ansible-playbook jenkins.yml -i inventories/stage --ask-vault-pass
+```
+### Workflow of Jenkins playbook
+* Builds core Jenkins image with custom plugins and packages. Pushes it to Dockerhub.
+* Installs Jenkins on kubernetes cluster using ansible k8s module. i.e applying role-binding, configmap, service, deployment etc
+* Builds Jenkins slave image and push it to Dockerhub
+
+## ELK Stack
+Elasticsearch, logstash, kibana and filebeats together are ELK stack. Installation includes three pod elasticsearch cluster, two logstash, one Kibana and one filebeat.
+Below is the architecture of ELK setup.
+
+![ELK Setup](https://github.com/vivekreddy94/document/blob/main/elk_architecture.png)
+
+### Overview of setup
+**Elasticsearch**: Three pod elasticsearch cluster is installed where they all talk to eachother to maintain replicas of incoming data and indices. As it is stateful application with requirement of persistent data, 'statefulsets' object is used which creates unique persisten identifier and recreates the pod with same identifier on failures. For data storage, persistent volumes are created and claimed through statefulset. Configuration is loaded to elasticsearch through configmaps.
+
+**Logstash**: Two pod logstash in installed to share incoming data load. Logstash is deployed through 'deployment' object and configuration is loaded through configmap. Logstash is configured to accept data on 5044 beats port from filebeats, then applies filters and other operations on data to finally send it to elasticsearch cluster.
+
+**Kibana**: One pod kibana is installed to visualize and quer data from elasticsearch. Similar to logstash, kibana is deployed via 'deployment' set and configuration is loaded through configmap.
+
+**filebeat**: One pod filebeat is installed to gather logs from docker containers and send it to logstash. For deploying filebeat 'daemonset' object is used as we want to pod to run all kubernetes cluster nodes. In this case only one pod is deployed, but on addition of new nodes to kuberenetes cluster, daemonset takes care of initiating pod on new nodes.
+
+## ELK deployment strategy
+![ELK stack deployment](https://github.com/vivekreddy94/document/blob/main/elk_jenkins_deploy.png)
+
+Workflow of ELK stack deployment in relation to above image
+1. Github notifies Jenkins elk CICD pipeline job on a new commit.
+2. Jenkins master using kubernetes plugin requests a slave pod.
+3. Jenkins slave pod is created dynamically by pulling image from docker registry
+4. CICD pipeline using kubeconfig file requests kubernetes api to deploy ELK stack
+5. ELK stack is deployed on kubernetes cluster
+
+### CICD Pipeline
+Below explains the steps performed in each stage of pipeline.
+
+![Pipeline stages](https://github.com/vivekreddy94/document/blob/main/cicd_arch.png)
+
+#### Checkout
+Checks out elk stack github repo.
+
+#### Test slave setup
+Checks the slave if required packages ansible, kubectl, docker, kubeval and polaris are installed as these are necessary for coming pipeline stages.
+
+#### Setup requirements
+* Copies the kubeconfig file from jenkins credentails to home directory in jenkins slave.
+* Executes kubernetes-lingting.yml playbook to copy ELK stack kubernetes files to slave node and make it ready for [Validate kubernetes code](https://github.com/vivekreddy94/document/blob/main/README.md#validate-kubernetes-code) stage
+
+#### Perform ansible linting
+* Builds a custom ansible-lint docker image by coping ansible files into the image
+* Runs docker image to perform linting on elk_stack.yml playbook
+* Finally cleans up ansible-linting docker image
+
+#### Validate kubernetes code
+* Kubernetes files are validated by [kubeval](https://kubeval.instrumenta.dev/) 
+* Next the files are validated by [polaris](https://github.com/FairwindsOps/polaris) for doing checks on images, tags, security priviliges, probes etc.
+
+#### Deploy and test elasticsearch
+* Deploys elasticsearch using ansible playbook
+```
+ansible-playbook elasticsearch.yml -i inventories/stage
+```
+* Checks if all pods in statefulset are ready
+* Loads sample data to elasticsearch cluster
+* Compares the loaded data with expected test output data.
+
+#### Deploy and test logstash
+* Deploys logstash using ansible playbook. For testing logstash, deployment includes 'http' plugin in logstash configuration to allow input data on 8080 port and creates output log file at /tmp/output.log
+```
+ansible-playbook logstash.yml -i inventories/stage
+```
+* Check if logstash pod is ready.
+* Loads sample data into logstash at 8080.
+* Tests if the output log file is created.
+
+#### Deploy and test filebeat
+* Deploys filebeat using ansible playbook.
+```
+ansible-playbook filebeat.yml -i inventories/stage
+```
+* Check if filebeat pod is ready.
+* Test filebeat configuration if its able to communicate with logstash
+
+#### Deploy and test kibana
+* Deploys kibana using ansible playbook.
+```
+ansible-playbook kibana.yml -i inventories/stage
+```
+* Check if kibana pod is ready
+* Check if kibana port is listening.
+
+#### Load data, test and cleanup
+* Executes data_loading_pods.yml playbook to deploy custom pods for generating 100000 random logs.
+* All the components are tested again after loading data.
+* Elasticsearch is queried to check if 100000 logs are loaded.
+* Finally, removes all components.
+```
+ansible-playbook elk_stack.yml -i inventories/stage --extra-vars "install_action=absent"
+```
+
+#### Deploy on production
+* Deploys [reloader](https://github.com/stakater/Reloader) for performing rolling upgrade on deployment, daemonset, and statefulsets.
+* Deploys elk_stack.yml playbook which includes elasticsearch, logstash, filebeat and kibana playbooks
+```
+ansible-playbook elk_stack.yml -i inventories/production
+```
+Note: Kubeconfig file of production cluster should be loaded here, but as it is a single node cluster kubeconfig file loaded in [Setup requirements](https://github.com/vivekreddy94/document/blob/main/README.md#setup-requirements) stage will be reused.
+
+
+
---
+++ jenkinsfile.groovy
@@ -0,0 +1,287 @@
+def print_debug_output(service){
+    sh(script: """
+           echo "#####Printing logs of ${service} for debugging#####"
+           kubectl describe pods -n elk -l app=${service}
+           kubectl logs -n elk -l app=${service}
+           """
+        )
+}
+
+def pod_readiness(service){
+    boolean success = false
+    counter = 0
+    while (!success) {
+        try{
+            if(counter<30){
+                ready_pods = sh(returnStdout: true, script: """kubectl get pods --sort-by=.status.startTime -n elk -l app=${service} | tail -n 1 | awk \'{print \$2}\'| cut -d \'/\' -f1""").trim()
+                if(ready_pods=='1'){
+                    success = true
+                }
+                else{
+                    counter = counter + 1
+                    sleep(30)
+                }
+            }
+            else{
+                throw new Exception("Taking too long")
+            }
+        }
+        catch(Exception e){
+            print_debug_output(service)
+            throw e
+        }
+    }
+}
+
+def statefulset_status(service,total_pods) {
+    boolean success = false
+    counter = 0
+    while (!success) {
+        try{
+            if(counter<30){
+                ready_pods = sh(returnStdout: true, script: """kubectl get sts -n elk -l app=${service} | grep ${service} | awk \'{ print \$2 }\' | cut -d\'/\' -f 1""").trim()
+                sleep(30)
+                if(ready_pods==total_pods){
+                    success = true
+                }
+                else{
+                    counter = counter + 1
+                }
+            }
+            else{
+                throw new Exception("Taking too long")
+            }
+        }
+        catch(Exception e){
+            print_debug_output(service)
+            throw e
+        }
+    }
+}
+
+
+def get_pod_name(service){
+    return sh(returnStdout: true, script: """kubectl get pod --sort-by=.status.startTime -n elk -l app=${service} | tail -n 1 | cut -d \' \' -f1""").trim()
+}
+
+def elasticsearch_testing(){
+    statefulset_status("elasticsearch","3")
+    try{
+        sh( script: """
+            echo "#### Testing elastic search data ####"
+            echo "#### Below is the test data #####"
+            cat test_data/elasticsearch/elastic_test_data
+            kubectl cp test_data/elasticsearch/elastic_test_data elasticsearch-0:/tmp/elastic_test_data -n elk
+            kubectl exec elasticsearch-0 -n elk -- curl -s -H \"Content-Type: application/x-ndjson\" -XPOST localhost:9200/_bulk --data-binary \"@/tmp/elastic_test_data\"; echo
+            """
+        )
+        sleep(60)
+        sh( script: """
+            echo "Query output data"
+            kubectl exec elasticsearch-0 -n elk -- curl -s -X GET \"localhost:9200/loading_data/_search?pretty\" | jq \".hits.hits\" | tee /tmp/elastic_output
+            diff test_data/elasticsearch/elastic_test_output /tmp/elastic_output
+            """
+        )
+    }
+    catch(Exception e){
+        print_debug_output("elasticsearch")
+        throw e
+    }
+}
+
+def logstash_testing(){
+    pod_readiness("logstash")
+    pod_name = get_pod_name("logstash")
+        try{
+            sh( script: """
+                echo "#### Testing logstash with test data ####"
+                echo "#### Below is the test data #####"
+                cat test_data/logstash/logstash_test_data
+                kubectl exec ${pod_name} -n elk -- rm -f /tmp/output.log
+                kubectl cp test_data/logstash/logstash_test_data ${pod_name}:/tmp/logstash_test_data -n elk
+                kubectl exec ${pod_name} -n elk -- curl -H \"content-type: application/json\" -XPUT \'http://127.0.0.1:8080/twitter/tweet/1\' -d \"@/tmp/logstash_test_data\"
+                echo "#### Check if output log is created #####"
+                kubectl exec ${pod_name} -n elk -- ls -l /tmp/output.log
+                """
+            )
+        }
+        catch(Exception e){
+            print_debug_output("logstash")
+            throw e
+        }
+}
+
+def kibana_testing(){
+    pod_readiness("kibana")
+    pod_name = get_pod_name("kibana")
+    try{
+        sh(script: """
+            echo "#### Testing Kibana port #####"
+            kubectl exec ${pod_name} -n elk -- curl http://localhost:5601/
+            """
+        )
+    }
+    catch(Exception e){
+        print_debug_output("kibana")
+        throw e
+    }
+}
+
+def filebeat_testing(){
+    pod_readiness("filebeat")
+    pod_name = get_pod_name("filebeat")
+    try{
+        sh(script: """
+            echo "#### Testing filebeat configuration #####"
+            kubectl exec ${pod_name} -n elk -- filebeat test output -c /etc/filebeat.yml
+            """
+        )
+    }
+    catch(Exception e){
+        print_debug_output("filebeat")
+        throw e
+    }
+}
+
+def data_loading_test(){
+    try{
+        int received_hits = sh(returnStdout: true, script: """
+        kubectl exec elasticsearch-0 -n elk -- curl -X GET \"elasticsearch-service:9200/logstash_index/_search?pretty\" -H \'Content-Type: application/json\' -d\' \
+        {
+        \"track_total_hits\": 10000000,
+        \"query\": {
+            \"match\": {
+            \"kubernetes.labels.app\": \"random-log-app-${BUILD_NUMBER}\"
+            }
+        }
+        }\' | jq \".hits.total.value\"
+        """
+        )
+        if (received_hits >= 100000){
+            echo "Recieved hits are matching loaded hits"
+        }
+        else {
+            throw e
+        }
+    }
+    catch(Exception e){
+        echo "Not able to retrieve the expected hits"
+        throw e
+    }
+}
+
+node('jenkins-slave') {
+    try {
+        stage('Checkout source code') {
+            git 'https://github.com/vivekreddy94/elk-kubernetes.git'
+        }
+        stage("Test slave setup") {
+            sh(script: """
+            echo "####check if docker is installed###"
+            docker version
+            echo "####Check if kubernetes is installed###"
+            kubectl version
+            echo "####Check if polaris is installed####"
+            which polaris
+            echo "#####Check if kubeval is installed####"
+            which kubeval
+            echo "##### Check if ansible is installed ###"
+            which ansible-playbook
+            """
+            )
+        }
+        stage("Setup requirements") {
+            withCredentials([file(credentialsId: 'kubeconfig', variable: 'configfile')]) {
+                sh(script: """
+                echo "####Setup kubeconfig file####"
+                rm -rf ~/.kube;mkdir ~/.kube;cp ${configfile} ~/.kube/config
+                echo "####Setup kubernetes yaml files for kubernetes code analysis####"
+                ansible-playbook kubernetes-linting.yml -i inventories/stage
+                """
+                )
+            }
+        }
+        stage('Perform ansible linting') {
+            echo "Building a image and performing ansible linting"
+            try{
+                sh "docker build -t vivekreddy94/ansible_lint:${BUILD_NUMBER} -f Dockerfile_lint ."
+                sh "docker run --rm vivekreddy94/ansible_lint:${BUILD_NUMBER} elk_stack.yml"
+            }
+            catch(e){
+                echo "Problem in performing ansible linting"
+                throw e
+            }
+            finally{
+                sh "docker rmi -f vivekreddy94/ansible_lint:${BUILD_NUMBER}"
+            }
+        }
+        stage("Validate kubernetes code"){
+            sh( script: """
+                echo "#### Perform kubeval to check validity####"
+                kubeval /tmp/*/final*.yml
+                echo "#### Perform polaris scan to check any missing setting #####"
+                polaris audit --config kube-code-analysis/config_elastisearch.yml --audit-path /tmp/elasticsearch/final-elastic.yml --set-exit-code-below-score 100
+                polaris audit --config kube-code-analysis/config_kibana.yml --audit-path  /tmp/kibana/final-kibana.yml --set-exit-code-below-score 100
+                polaris audit --config kube-code-analysis/config_filebeat.yml --audit-path /tmp/filebeat/final-filebeat.yml --set-exit-code-below-score 100
+                polaris audit --config kube-code-analysis/config_logstash.yml --audit-path /tmp/logstash/final-logstash.yml --set-exit-code-below-score 100
+                """
+            )
+        }
+
+        stage("Deploy and test elasticsearch") {
+            sh "ansible-playbook elasticsearch.yml -i inventories/stage"
+            sleep(30)
+            elasticsearch_testing()
+        }
+
+        stage("Deploy and test logstash") {
+            sh "ansible-playbook logstash.yml -i inventories/stage"
+            sleep(30)
+            logstash_testing()
+        }
+        stage("Deploy and test filebeat") {
+            sh "ansible-playbook filebeat.yml -i inventories/stage"
+            sleep(30)
+            filebeat_testing()
+        }
+        stage("Deploy and test kibana") {
+            sh "ansible-playbook kibana.yml -i inventories/stage"
+            sleep(30)
+            kibana_testing()
+        }
+        stage('Load data, test and cleanup') {
+            try{
+                sh "ansible-playbook data_loading_pods.yml --extra-vars \"build_number=${BUILD_NUMBER} install_action=present\" -i inventories/stage/"
+                sleep(180)
+                echo "##### Testing all components after loading data #####"
+                elasticsearch_testing()
+                logstash_testing()
+                filebeat_testing()
+                kibana_testing()
+                data_loading_test()
+            }
+            catch (e){
+                echo "Testing failed after loading data"
+                throw e
+            }
+            finally{
+                echo "#### cleaning up data loading apps ####"
+                sh "ansible-playbook data_loading_pods.yml --extra-vars \"build_number=${BUILD_NUMBER} install_action=absent\" -i inventories/stage/"
+                sh "ansible-playbook elk_stack.yml -i inventories/stage --extra-vars \"install_action=absent\""
+            }
+
+        }
+        stage("Deploy to prod") {
+            sh "ansible-playbook config-reloader.yml -i inventories/stage"
+            sh "ansible-playbook elk_stack.yml -i inventories/production"
+        }
+    }
+    catch (e) {
+        throw e
+    }
+    finally {
+        sh "ansible-playbook elk_stack.yml -i inventories/stage --extra-vars \"install_action=absent\""
+        sh 'rm -rf ~/.kube/config'
+    }
+}
+
